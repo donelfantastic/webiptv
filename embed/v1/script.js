a346608ee6dbb64b42f83e4811cc6805fad1ef29
Donel Fantastic <121449182+donelfantastic@users.noreply.github.com>
2024-03-09 21:42:52 +0700

    Update script.js

--- embed/v1/script.js
+++ embed/v1/script.js
@@ -1,2 +1,16 @@
-const currentLocation = window.location.search;
-const sURL = currentLocation.replace("?live&url=", "");
+document.addEventListener("DOMContentLoaded", () => {
+const video = document.querySelector("#my-player");
+if (video.canPlayType("application/vnd.apple.mpegurl")) {
+  video.src = "https://donelfantastic.github.io/webiptv/web/" + sURL;
+} else if (Hls.isSupported()) {
+  const hls = new Hls({
+    maxBufferSize: 1 * 1000,
+  });
+  hls.loadSource(src);
+  hls.attachMedia(video);
+} else {
+  console.error(
+    "This is a legacy browser that doesn't support Media Source Extensions"
+  );
+}
+});
